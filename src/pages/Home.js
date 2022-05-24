4831f36205e1180e6dbd9577bd4dbab243b8c567
Henrique Ramos <henriqueframos@ua.pt>
2022-05-24 22:19:23 +0100

    nova homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 //import React from 'react'
 import {Link} from "react-router-dom"
-import BannerImage from '../assets/background.jpeg'
+import BannerImage from '../assets/fundo.jpeg'
 import '../styles/Home.css'
 import * as React from 'react';
 import Card from '@mui/material/Card';
@@ -22,10 +22,10 @@ function Home() {
                 <h1>What are you trying to fix?</h1>
             </div>
             <div className="button_container">
-                <button class="button_phone" role="button">Phones</button>
-                <button class="button_tablet" role="button">Tablets</button>
-                <button class="button_pc" role="button">PCs</button>
-                <button class="button_watch" role="button">SmartWatches</button>
+                <button class="button_phone" role="button"></button>
+                <button class="button_tablet" role="button"></button>
+                <button class="button_pc" role="button"></button>
+                <button class="button_watch" role="button"></button>
                 {/* <button class="button_phone" role="button">phones</button>
                 <button class="button_phone" role="button">phones</button> */}
             </div>
