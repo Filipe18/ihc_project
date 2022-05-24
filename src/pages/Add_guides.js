dfc5c23b55a94c46632e79c12f5c35c4c3e21b63
Renato <renatomaourives@ua.pt>
2022-05-24 09:54:38 +0100

    add_guidesv1

--- src/pages/Add_guides.js
+++ src/pages/Add_guides.js
@@ -2,19 +2,79 @@ import React from 'react'
 import {Link} from "react-router-dom"
 import BannerImage from '../assets/background4.jpg'
 import '../styles/Home.css'
+import countries from "./countries";
+import './style.css'
+
 
 function Add_guides() {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [country, setCountry] = React.useState("");
+    const [acceptedTerms, setAcceptedTerms] = React.useState(false);
+
+    const handleSubmit = (event) => {
+        console.log(`
+        Email: ${email}
+        Password: ${password}
+        Country: ${country}
+        Accepted Terms: ${acceptedTerms}
+        `);
+
+        event.preventDefault();
+  }
+
     return (
-        <div className="home"  style={{backgroundImage: `url(${BannerImage})`}}>
-            <div className="headerContainer">
-                <h1>Self-Repair</h1>
-                <Link to="/guides">
-                <button>See the guides</button>
-                </Link>
-            </div>
-    
-        </div>
-    )
+        <div className="home" style={{backgroundImage: `url(${BannerImage})`}}>            
+            <form onSubmit={handleSubmit}>
+      <h1>Create Account</h1>
+
+      <label>
+        Email:
+        <input
+          name="email"
+          type="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          required />
+      </label>
+
+      <label>
+        Password:
+        <input
+          name="password"
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          required />
+      </label>
+
+      <label>
+        Country:
+        <select
+          name="country"
+          value={country}
+          onChange={e => setCountry(e.target.value)}
+          required>
+          <option key=""></option>
+          {countries.map(country => (
+            <option key={country}>{country}</option>
+          ))}
+        </select>
+      </label>
+
+      <label>
+        <input
+          name="acceptedTerms"
+          type="checkbox"
+          onChange={e => setAcceptedTerms(e.target.value)}
+          required />
+        I accept the terms of service
+      </label>
+
+      <button>Submit</button>
+    </form>
+    </div>
+    );
 }
 
 export default Add_guides
